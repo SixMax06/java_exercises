681b266d3e2d5e36be3463f8d51eb38b869f316c
SixMax06 <alemilamax@gmail.com>
2024-12-18 10:47:30 +0100

    Completato esercizio "pallavolo"

--- JavaFX/pallavolo/src/main/java/sixmax06/javafx/pallavolo/Contatore.java
+++ JavaFX/pallavolo/src/main/java/sixmax06/javafx/pallavolo/Contatore.java
@@ -3,18 +3,53 @@ package sixmax06.javafx.pallavolo;
 public class Contatore {
     private int set;
     private int punteggio;
-    private final int MAX;
+    private int MAX;
+    private boolean vittoria;
 
     public Contatore(int max) {
         this.set = 0;
         this.punteggio = 0;
         this.MAX = max;
+        this.vittoria = false;
     }
 
     public void incrementa() {
         this.punteggio++;
-        this.set += this.punteggio / MAX;
-        this.punteggio %= this.MAX;
+        if (this.punteggio == MAX) {
+            this.punteggio = 0;
+            this.set++;
+            this.vittoria = true;
+        }
+    }
+
+    public int getPunteggio() {
+        return this.punteggio;
+    }
+
+    public int getMAX() {
+        return this.MAX;
+    }
+
+    public void setMAX(int max) {
+        this.MAX = max;
+    }
+
+    public boolean isVittoria() {
+        return this.vittoria;
+    }
+
+    public void resetVittoria() {
+        this.vittoria = false;
+    }
+
+    public void resetPunteggio() {
+        this.punteggio = 0;
+    }
+
+    public void reset() {
+        this.set = 0;
+        this.punteggio = 0;
+        this.vittoria = false;
     }
 
     @Override
--- JavaFX/pallavolo/src/main/java/sixmax06/javafx/pallavolo/PallavoloController.java
+++ JavaFX/pallavolo/src/main/java/sixmax06/javafx/pallavolo/PallavoloController.java
@@ -7,25 +7,51 @@ import javafx.event.ActionEvent;
 
 public class PallavoloController {
     private Contatore casa, trasferta;
+    private final int MAX = 25;
 
     @FXML
     private Label lblCasa, lblTrasferta;
 
     @FXML
     public void initialize() {
-        this.casa = new Contatore(25);
-        this.trasferta = new Contatore(25);
+        this.casa = new Contatore(MAX);
+        this.trasferta = new Contatore(MAX);
         this.lblCasa.setText(casa.toString());
         this.lblTrasferta.setText(trasferta.toString());
     }
 
+    private void checkVantaggio() {
+        if (this.casa.getPunteggio() == this.casa.getMAX() - 1 && this.trasferta.getPunteggio() == this.trasferta.getMAX() - 1) {
+            this.casa.setMAX(this.casa.getMAX() + 1); this.trasferta.setMAX(this.trasferta.getMAX() + 1);
+        }
+    }
+
     public void btnCasaOnClick(ActionEvent actionEvent) {
         this.casa.incrementa();
+        if (this.casa.isVittoria()) {
+            this.trasferta.resetPunteggio();
+            this.casa.resetVittoria();
+            this.casa.setMAX(this.MAX); this.trasferta.setMAX(this.MAX);
+            this.lblTrasferta.setText(trasferta.toString());
+        }
+        this.checkVantaggio();
         this.lblCasa.setText(casa.toString());
     }
 
     public void btnTrasfertaOnClick(ActionEvent actionEvent) {
         this.trasferta.incrementa();
+        if (this.trasferta.isVittoria()) {
+            this.casa.resetPunteggio();
+            this.trasferta.resetVittoria();
+            this.casa.setMAX(this.MAX); this.trasferta.setMAX(this.MAX);
+            this.lblCasa.setText(casa.toString());
+        }
+        this.checkVantaggio();
         this.lblTrasferta.setText(trasferta.toString());
+    }
+
+    public void btnResetOnClick(ActionEvent actionEvent) {
+        this.casa.reset(); this.trasferta.reset();
+        this.lblCasa.setText(casa.toString()); this.lblTrasferta.setText(trasferta.toString());
     }
 }
