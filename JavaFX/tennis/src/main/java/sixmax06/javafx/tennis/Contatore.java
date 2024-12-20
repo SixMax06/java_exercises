6b631fa616d68c3eff1fad7c17859b1ff55559f8
SixMax06 <alemilamax@gmail.com>
2024-12-20 18:04:00 +0100

    piccolo aggiornamento del codice

---
+++ JavaFX/tennis/src/main/java/sixmax06/javafx/tennis/Contatore.java
@@ -0,0 +1,11 @@
+package sixmax06.javafx.tennis;
+
+public class Contatore {
+    private int punteggio, set, game;
+
+    public Contatore() {
+        punteggio = 0;
+        set = 0;
+        game = 0;
+    }
+}
---
+++ JavaFX/tennis/src/main/java/sixmax06/javafx/tennis/Giocatore.java
@@ -0,0 +1,12 @@
+package sixmax06.javafx.tennis;
+
+public class Giocatore {
+    private int punteggio, set, game;
+
+    public Giocatore() {
+        punteggio = 0;
+        set = 0;
+        game = 0;
+    }
+
+}
